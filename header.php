7e96e29232ac8945563286ff0893797a1471476a
Rameen Zeeshan <rameen.zeeshanedu@gmail.com>
2025-08-19 20:48:51 +0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Rehan School Clone</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header>
+    <h1>Rehan School 🌌</h1>
+    <nav>
+      <a href="index.php">Home</a>
+      <a href="curriculum.php">Curriculum</a>
+      <a href="facilitators.php">Facilitators</a>
+      <a href="contact.php">Contact</a>
+    </nav>
+  </header>
+  <main>
+ 
