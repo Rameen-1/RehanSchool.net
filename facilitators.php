3eab7aad09e1bb0f533aee2af3c0fbf4e0ebd9bb
Rameen Zeeshan <rameen.zeeshanedu@gmail.com>
2025-08-19 20:47:55 +0400

    Create facilitators.php

---
+++ facilitators.php
@@ -0,0 +1,27 @@
+<?php include 'header.php'; ?>
+ 
+<section class="facilitators">
+  <h1>Meet Our Changemakers</h1>
+  <div class="profile">
+    <img src="assets/images/facilitator1.jpg" alt="Ms. Ayesha">
+    <h3>Ms. Ayesha</h3>
+    <p>Head of AI Education</p>
+    <a href="#">LinkedIn</a>
+  </div>
+  <div class="profile">
+    <img src="assets/images/facilitator2.jpg" alt="Mr. Rehan">
+    <h3>Mr. Rehan</h3>
+    <p>Founder & Entrepreneur Coach</p>
+    <a href="#">Twitter</a>
+  </div>
+  <div class="profile">
+    <img src="assets/images/facilitator3.jpg" alt="Sir Imran">
+    <h3>Sir Imran</h3>
+    <p>Community Coordinator</p>
+    <a href="#">Facebook</a>
+  </div>
+</section>
+ 
+ 
+<?php include 'footer.php'; ?>
+ 
